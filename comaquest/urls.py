0628795a42c067ac1fa82a5ae6d0ccb2bf57e849
franchuk <xcorter@mail.ru>
2014-12-20 17:35:12 +0600

    первый коммит, немного верстки

---
+++ comaquest/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'comaquest.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^', include('main.urls', namespace="main")),
+    url(r'^admin/', include(admin.site.urls)),
+)
