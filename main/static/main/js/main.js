a2421359b6dc696218efee459175a4473ca2c124
franchuk <xcorter@mail.ru>
2015-01-06 20:27:20 +0600

    переделал заказ

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -30,7 +30,13 @@ $(document).ready(function(){
 
     function disableTimeOrders() {
         $('.time input[name=time]').prop('checked', false).prop('disabled', false);
-        var targetDate = $("#datepicker").datepicker({ dateFormat: 'dd-mm-yy' }).val();
+        var $datepicker = $("#datepicker");
+        var targetDate = $datepicker.datepicker({ dateFormat: 'dd-mm-yy' }).val();
+        var DD = $.datepicker.formatDate('DD', new Date(targetDate));
+        var monthNum = $datepicker.datepicker('getDate').getMonth();
+        var dayNum = $datepicker.datepicker('getDate').getDate();
+        var text = dayNum + " " + genitive[monthNum] + ", " + DD;
+        $('.fields .date').text(text);
         $('input[name=date]').val(targetDate);
         for (var i = 0; i < orders.length; i++) {
             if (
@@ -46,15 +52,64 @@ $(document).ready(function(){
         }
     }
 
-    $.datepicker.setDefaults($.extend($.datepicker.regional["ru"]));
-    $("#datepicker").datepicker({
-        dateFormat: "yy-mm-dd",
-        minDate: 0,
-        maxDate: "+1M +10D",
-        onSelect: function (date, inst) {
-            disableTimeOrders();
+    function addAvailable(refresh) {
+        if (refresh) {
+            $("#datepicker").datepicker("refresh");
         }
-    });
+        var selectedQuest = $('[name=quest] option:selected');
+        var times = null;
+        for (var i = 0; i < schedule.length; i++) {
+            if (schedule[i].questId == selectedQuest.val()) {
+                times = schedule[i].times.length;
+                break;
+            }
+        }
+        var tdDate = null;
+        $("#datepicker").find('td:not(.ui-state-disabled)').each(function(i, el){
+            var currentTimes = times;
+            var available = 0;
+            for (var i = 0; i < orders.length; i++) {
+                tdDate =
+                    $.datepicker.formatDate(
+                        "yy-mm-dd",
+                        new Date(
+                            $(el).data('year'),
+                            $(el).data('month'),
+                            $(el).text()
+                        )
+                    );
+                if (
+                    orders[i].fields.date == tdDate &&
+                        orders[i].fields.quest == selectedQuest.val()
+                ) {
+                    currentTimes--;
+                }
+            }
+            $(this).find('.ui-state-default').attr('data-available', currentTimes);
+        });
+    }
+
+    $.datepicker.setDefaults($.extend($.datepicker.regional["ru"]));
+    $("#datepicker")
+        .datepicker({
+            dateFormat: "yy-mm-dd",
+            minDate: 0,
+            maxDate: "+6D",
+            onSelect: function (date, inst) {
+                disableTimeOrders();
+                setTimeout(
+                    function() {addAvailable(false)},
+                    50
+                );
+            },
+            onChangeMonthYear: function() {
+                disableTimeOrders();
+                setTimeout(
+                    function() {addAvailable(false)},
+                    50
+                );
+            }
+        });
 
     $('.submit').click(function() {
         event.preventDefault();
@@ -85,6 +140,7 @@ $(document).ready(function(){
     $('select.quest').change(function(){
         setTimes();
         disableTimeOrders();
+        addAvailable(true);
     });
 
     function setTimes() {
@@ -105,6 +161,7 @@ $(document).ready(function(){
     function init() {
         setTimes();
         disableTimeOrders();
+        addAvailable(false);
     }
     init();
 });
